1c83f416794aa0e5507267ec1769c2dcd9259fe5
samuel <samuka.jvm@gmail.com>
2026-04-07 10:28:19 -0400

    Operadores Relacionais

---
+++ projeto1-java-basico/src/modulo01/aula06/OperadoresRelacionais.java
@@ -0,0 +1,15 @@
+package modulo01.aula06;
+
+public class OperadoresRelacionais {
+    /* Curso de Java 13: Operadores (Aritiméticos, Lógicos e Relacionais) */
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+        System.out.println("numero1 == numero2: " + (numero1 == numero2));
+        System.out.println("numero1 != numero2: " + (numero1 != numero2));
+        System.out.println("numero1 > numero2: " + (numero1 > numero2));
+        System.out.println("numero1 >= numero2: " + (numero1 >= numero2));
+        System.out.println("numero1 < numero2: " + (numero1 < numero2));
+        System.out.println("numero1 <= numero2: " + (numero1 <= numero2));
+    }
+}
