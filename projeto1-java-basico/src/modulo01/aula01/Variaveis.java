a9b5e528c257c5a0b1d38932a69b939269fa4801
samuel <samuka.jvm@gmail.com>
2026-04-07 10:26:26 -0400

    Introdução a Variávels

---
+++ projeto1-java-basico/src/modulo01/aula01/Variaveis.java
@@ -0,0 +1,15 @@
+package modulo01.aula01;
+
+public class Variaveis {
+    /* Curso de Java 10: Introdução a Variávels */
+    public static void main(String[] args) {
+        int idade = 20;
+        String nome = "Samy";
+        String nomeDoMeuCahorro = "totó";
+        idade = 25;
+        System.out.println("Nome = " + nome);
+        System.out.println("Idade = " + idade);
+        int a = 10;
+        String b = "Raio";
+    }
+}
